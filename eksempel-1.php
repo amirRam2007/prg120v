9701bcd59e482de73f8d04436605a928bed0f2f2
amirRam2007 <am390359@gmail.com>
2026-09-07 15:31:30 +0200

    Correct PHP syntax and improve comments
    Fix formatting and comments in eksempel-1.php

--- eksempel-1.php
+++ eksempel-1.php
@@ -1,1 +1,11 @@
+<?php /* Eksempe 1 */
+/*
+/* programmet mottar fr et HTML-skjema et fornavn og et etternavn ved POST.metoden 
+/* programmet skriver ut en "god dag"-melding med personens navn 
+*/
+ $fornavn=$_POST ["fornavn];
+ $etternavn=$_POST ["etternavn];
+
+print ("god dag amir $fornavn $etternavn <br />");
+?>
 
