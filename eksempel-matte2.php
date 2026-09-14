65211754a6bb620fe568c2dce1f6e3914576bcae
amirRam2007 <am390359@gmail.com>
2026-09-14 17:15:12 +0200

    Implement answer checking for multiplication question
    The program checks the answer to the question 'what is 3 times 3?' and outputs a message based on the user's input.

--- eksempel-matte2.php
+++ eksempel-matte2.php
@@ -1,1 +1,22 @@
-
+<?php  /*eksempel-matte2.php */
+/*
+/* Programmet mottar fra et HTML-skjema et svar på spørmålet "hva er 3 ganger 3 ?"
+/* Programmet sjekker om svaret er riktig og skirver ut en melding ang. svaret 
+/* Meldingen skrives ut på samme side som HTML-skemaet er 
+*/ 
+if (isset($_POST ["forsett"]))
+{
+  $svar=$_POST ["svar"];
+ 
+  if ($svar == 9)
+  {
+    print("riktig. 3 ganger 3 er 9 ");
+  }
+  else
+  {
+    print ("feil. 3 ganger 3 er ikke $svar. 3 ganger 3 er 9 ");
+  }
+}
+?>
+  
+    
